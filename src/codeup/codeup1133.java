5d5e47df17bd5bb2fb9eb2c5533e38c7a1a55583
YJ <simyj547@gmail.com>
2021-12-16 10:02:55 +0900

    211216 codeup commit

---
+++ src/codeup/codeup1133.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+// 1133 : 공백이 있는 문자열 입출력
+// 공백이 포함된 문자열이 입력된다. (최대길이 30)
+public class codeup1133 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String c = sc.nextLine();
+		System.out.println(c);
+	}
+}
---
+++ src/codeup/codeup1137.java
@@ -0,0 +1,21 @@
+package codeup;
+
+import java.util.Scanner;
+
+// 1137 : 관계연산자 3
+// 두 정수 a, b가 공백을 두고 입력된다.(a,b는 int형 범위)
+// a와 b가 같지 않을 경우 1, 그렇지 않은 경우 0을 출력한다.
+public class codeup1137 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a!=b) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+				
+	}
+}
