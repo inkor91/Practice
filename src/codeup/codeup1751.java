8c85a0dd617605860f1af7dff3662376e2390b9b
YJ <simyj547@gmail.com>
2022-04-21 21:45:37 +0900

    220421 daily practice commit

---
+++ src/codeup/codeup1751.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+// 1751 : 글자수 세기
+// 공백을 포함하여 문자열의 길이를 출력한다.
+
+public class codeup1751 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		
+		System.out.println(line.length());
+	}
+}
