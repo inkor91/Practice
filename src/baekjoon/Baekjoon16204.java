b83763ca9d3e5998d9aa4da373ba9a963383a525
inkor91 <simyj547@gmail.com>
2022-05-15 16:22:58 +0900

    220515 daily practice commit

---
+++ src/baekjoon/Baekjoon16204.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon16204 {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int ans = Math.min(m, k) + Math.min(n-m, n-k);
+        System.out.println(ans);
+	}
+}
