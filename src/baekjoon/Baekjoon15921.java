c1bf8de0edff208730d0ece04ab67848f79971f4
inkor91 <simyj547@gmail.com>
2022-05-24 23:10:28 +0900

    daily practice commit

--- src/baekjoon/Baekjoon15921.java
+++ src/baekjoon/Baekjoon15921.java
@@ -1,5 +1,15 @@
 package baekjoon;
 
-public class Baekjoon15921 {
+import java.util.Scanner;
 
+public class Baekjoon15921 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n!=0) {
+			System.out.println("1.00");
+		}else {
+			System.out.println("divide by zero");
+		}
+	}
 }
