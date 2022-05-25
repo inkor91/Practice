b2d44023550eab5399d56ab434debf18b0056d91
inkor91 <simyj547@gmail.com>
2022-05-25 23:06:50 +0900

    220525 daily practice commit

---
+++ src/baekjoon/Baekjoon16199.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon16199 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y1 = Integer.parseInt(sc.next());
+		int m1 = Integer.parseInt(sc.next());
+		int d1 = Integer.parseInt(sc.next());
+		int y2 = Integer.parseInt(sc.next());
+		int m2 = Integer.parseInt(sc.next());
+		int d2 = Integer.parseInt(sc.next());
+		int age = y2 - y1;
+		
+		if ( y1 == y2) {
+			System.out.println(age);
+		}else {
+			if (m1 > m2) {
+				System.out.println(age -1);
+			}else if (m1 == m2) {
+				if (d1 <= d2) {
+					System.out.println(age);
+				}else {
+					System.out.println(age -1);
+				}
+			}else {
+				System.out.println(age);
+			}
+		}
+		System.out.println(age + 1);
+		System.out.println(age);
+	}
+}
