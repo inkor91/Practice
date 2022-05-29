993ae75b6bee90748cf93ed279b635d7b8a7af21
inkor91 <simyj547@gmail.com>
2022-05-29 13:05:03 +0900

    220529 daily practice commit

---
+++ src/baekjoon/Baekjoon25083.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+public class Baekjoon25083 {
+	public static void main(String[] args) {
+			String s = "         ,r'\"7\n" + 
+					   "r`-_   ,'  ,/\n" + 		
+					   " \\. \". L_r'\n" + 	
+					   "   `~\\/\n" + 			
+					   "      |\n" + 	
+					   "      |";
+	 
+			System.out.print(s);
+		}
+}
