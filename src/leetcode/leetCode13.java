c1f04106d125ab267af7e2f5771c20e775bcb21d
inkor91 <simyj547@gmail.com>
2022-07-21 21:32:17 +0900

    modify

--- src/leetcode/leetCode13.java
+++ src/leetcode/leetCode13.java
@@ -10,26 +10,28 @@ package leetcode;
 
 public class leetCode13 {
 	public int romanToInt(String s) {
-		int num = 0;
-		int len = s.length();
-		char c;
-
-		for (int i = len - 1; i >= 0; i--) {
-			c = s.charAt(i);
-			if (c == 'I') {
-				num += (num >= 5 || num >= 10) ? -1 : 1;
-			} else if (c == 'V') {
-				num += 5;
-			} else if (c == 'X') {
-				num += (num >= 50 || num >= 100) ? -10 : 10;
-			} else if (c == 'L') {
-				num += 50;
-			} else if (c == 'D') {
-				num += 500;
-			} else if (c == 'M') {
-				num += 1000;
-			}
-		}
-		return num;
-	}
+        int num = 0;
+        int len = s.length();
+        char c;
+        
+        for (int i = len - 1; i >= 0; i--) {
+            c = s.charAt(i);
+            if (c == 'I') {
+                num += (num >= 5 || num >= 10) ? -1 : 1;
+            } else if (c == 'V') {
+                num += 5;
+            } else if (c == 'X') {
+                num += (num >= 50 || num >= 100) ? -10 : 10;
+            } else if (c == 'L') {
+                num += 50;
+            } else if (c == 'C') {
+                num += (num >= 500 || num >= 1000) ? -100 : 100;
+            } else if (c == 'D') {
+                num += 500;
+            } else if (c == 'M') {
+                num += 1000;
+            }
+        }
+        return num;
+    }
 }
